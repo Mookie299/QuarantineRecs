058f98d9b5474bdc26956fd4591680c39e85234a
Daniella Billini <Daniellabillini@gmail.com>
2020-06-17 15:02:50 -0400

    favorites page

---
+++ QurantineRecs/app/src/main/java/com/example/quarantinerecs/Favorites.java
@@ -0,0 +1,4 @@
+package com.example.quarantinerecs;
+
+public class Favorites {
+}
--- QurantineRecs/app/src/main/java/com/example/quarantinerecs/MainActivity.java
+++ QurantineRecs/app/src/main/java/com/example/quarantinerecs/MainActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private  final
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
