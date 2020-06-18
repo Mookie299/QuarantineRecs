963cddd399866b1199c76dfbe56436268280cd9b
Daniella Billini <Daniellabillini@gmail.com>
2020-06-18 00:52:53 -0400

    movies page added

--- QurantineRecs/app/src/main/java/com/example/quarantinerecs/MainActivity.java
+++ QurantineRecs/app/src/main/java/com/example/quarantinerecs/MainActivity.java
@@ -17,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //hello , how are you today?
-        // hello, my name is Daniella.
-        //my cat is a crackhead
-        //his middle name is Barry like the flash
-
-  
-       //hey
-
         img = findViewById(R.id.blackLives);
 
     }
