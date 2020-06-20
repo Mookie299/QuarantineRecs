0292cb5a518078e574971dbb3d6b35f490749e8c
Daniella Billini <Daniellabillini@gmail.com>
2020-06-20 12:07:14 -0400

    something's wrong

---
+++ QurantineRecs/app/src/main/java/com/example/quarantinerecs/blmActivity.java
@@ -0,0 +1,42 @@
+package com.example.quarantinerecs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class blmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blm);
+    }
+
+    public void goToSo (View view) {
+        goToUrl ( "http://stackoverflow.com/");
+    }
+
+    public void goToSu (View view) {
+        goToUrl ( "http://superuser.com/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void goToScreen3() {
+        Intent toRes = new Intent(this, blmActivity.class);
+        startActivity(toRes);
+
+    }
+
+    public void goToRes(View view) {
+        goToScreen3();
+    }
+}
+
