f831a9a1d5ffe2cc4bf573939bf7d6821a05d692
admin <admin@MacBook-Pro-10.home>
2020-06-20 14:24:16 -0400

    final touches for now

--- QurantineRecs/app/src/main/java/com/example/quarantinerecs/blmActivity.java
+++ QurantineRecs/app/src/main/java/com/example/quarantinerecs/blmActivity.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class blmActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blm);
+
+        btn = findViewById(R.id.clickMe);
+        
     }
 
     public void goToSo(View view) {
